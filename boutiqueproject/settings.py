8970d45a6993e00ffb77d3cef48991604288d077
totalnoMartina <totalnobzvz@yahoo.com>
2022-03-20 22:45:44 +0000

    Add shopping bag app, urls, view and template

--- boutiqueproject/settings.py
+++ boutiqueproject/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     #      'allauth.socialaccount.providers.google',
     'home',
-    'products'
+    'products',
+    'bag',
 ]
 
 #  Provider specific settings
