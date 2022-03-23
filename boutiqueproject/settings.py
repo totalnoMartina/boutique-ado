e60a824d8468d96753c028108733b663e1c34de5
totalnoMartina <totalnobzvz@yahoo.com>
2022-03-23 13:48:59 +0000

    Add secret key to heroku, development in os.environ

--- boutiqueproject/settings.py
+++ boutiqueproject/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zx+i_&2n^7yfr0g7g*!7ey91f3@s^7bx3&+s#vc9@_nap9m867'
+SECRET_KEY = os.environ.get("SECRET_KEY", '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['boutique-martina.herokuapp.com', 'localhost']
 
