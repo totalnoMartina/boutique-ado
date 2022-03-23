ac9be0a8ad37ac9df866b4c510fb362b0f35a143
totalnoMartina <totalnobzvz@yahoo.com>
2022-03-23 13:40:46 +0000

    Install gunicorn, psycopgy-bin, dj_dtbase_url, allowed hosts for heroku deploy

--- boutiqueproject/settings.py
+++ boutiqueproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-zx+i_&2n^7yfr0g7g*!7ey91f3@s^7bx3&+s#vc9@_nap9m867
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['outique-martina.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -137,13 +138,17 @@ WSGI_APPLICATION = 'boutiqueproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
