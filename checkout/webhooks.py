53c9a3dca43f996c644c263005a972ec61b19679
totalnoMartina <totalnobzvz@yahoo.com>
2022-04-18 21:25:13 +0000

    Add comment for other errors

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -21,8 +21,8 @@ def webhook(request):
 
     try:
         event = stripe.Webhook.construct_event(
-        payload, sig_header, wh_secret
-        )
+            payload, sig_header, wh_secret
+                )
     except ValueError as e:
         # Invalid payload
         return HttpResponse(status=400)
@@ -30,6 +30,7 @@ def webhook(request):
         # Invalid signature
         return HttpResponse(status=400)
     except Exception as e:
+        # Other Types of error
         return HttpResponse(content=e, status=400)
 
     # Set up a webhook handler
